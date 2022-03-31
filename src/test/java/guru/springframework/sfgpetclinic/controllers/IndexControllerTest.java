ed99ad6a04ad9a283cd72e1f723fb8bcceea2c8e
Nicu Taban <nictbndeveloper@gmail.com>
2022-03-31 23:38:49 +0300

    JUnit Assumptions example

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -50,5 +51,15 @@ class IndexControllerTest {
             Thread.sleep(2000);
             System.out.println("I got here but differently");
         });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionTrueButItReallyIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
     }
 }
