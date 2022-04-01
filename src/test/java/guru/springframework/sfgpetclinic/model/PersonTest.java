79959017b3e8b787947e1b40b84b3bb62c9ed382
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-01 22:08:58 +0300

    Example of Dependency Injection in JUnit

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +32,10 @@ class PersonTest implements ModelTests {
     @DisplayName("My Repeated Test")
     void myRepeatedTest() {
         // TODO
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDependencyInjection(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + " : " + repetitionInfo.getCurrentRepetition());
     }
 }
